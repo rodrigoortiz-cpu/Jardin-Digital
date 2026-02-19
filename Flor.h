bd16631014d187fd7f93ca09169e490ab0d067f5
rodrigoortiz-cpu <rodrigo.ortiz@utec.edu.pe>
2026-02-19 10:11:37 -0500

    Update Flor.h

--- Flor.h
+++ Flor.h
@@ -10,4 +10,54 @@ public:
     int get_tam();
     void dibujar(CImg<unsigned char>& img);
 };
+
 #endif //JARDINPROYECTOPROGRA2_FLOR_H
+
+#ifndef PETALO_H
+#define PETALO_H
+
+class Petalo {
+private:
+    float radio;
+    int colorR, colorG, colorB;
+
+public:
+    Petalo(float radio, int r, int g, int b) {
+        this->radio = radio;
+        this->colorR = r;
+        this->colorG = g;
+        this->colorB = b;
+    }
+    float getRadio() { return radio; }
+    int getR() { return colorR; }
+    int getG() { return colorG; }
+    int getB() { return colorB; }
+    void dibujar() {
+    }
+};
+
+#endif
+
+#ifndef CENTRO_H
+#define CENTRO_H
+
+class Centro {
+private:
+    float radio;
+    int colorR, colorG, colorB;
+public:
+    Centro(float radio, int r, int g, int b) {
+        this->radio = radio;
+        this->colorR = r;
+        this->colorG = g;
+        this->colorB = b;
+    }
+    float getRadio() { return radio; }
+    int getR() { return colorR; }
+    int getG() { return colorG; }
+    int getB() { return colorB; }
+    void dibujar() {
+    }
+};
+
+#endif
